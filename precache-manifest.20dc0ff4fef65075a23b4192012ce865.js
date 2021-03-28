7b3a1baea673462e056223c9242f6579ba5ebb5e
Code Sandbox <hello@codesandbox.io>
2021-03-28 12:05:57 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.20dc0ff4fef65075a23b4192012ce865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3659582c12a3e2baf7399eb341f3d6",
+    "url": "https://enid-colic.github.io/csb-zho2c/index.html"
+  },
+  {
+    "revision": "223b16f1df9836b8f30c",
+    "url": "https://enid-colic.github.io/csb-zho2c/static/js/2.ba2660bc.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "https://enid-colic.github.io/csb-zho2c/static/js/2.ba2660bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1d8e4ac4a9ae039a8a",
+    "url": "https://enid-colic.github.io/csb-zho2c/static/js/main.6d7b6d22.chunk.js"
+  },
+  {
+    "revision": "4d7624b7bc1fa19919bb",
+    "url": "https://enid-colic.github.io/csb-zho2c/static/js/runtime-main.aab42b2f.js"
+  }
+]);
